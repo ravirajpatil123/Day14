9ec6d0c8655e97507e81a8ceefad42d2a6e5d067
Raviraj patil <ravirajpatil298@gmail.com>
2023-06-13 10:33:03 +0530

    object

--- second-react-app/src/App.js
+++ second-react-app/src/App.js
@@ -1,20 +1,14 @@
 function App(){
   let str =`hello ravi`;
   let pi=3.14;
-  let active=true;
-  let isUserLoggedIn = true;
+  let user = { id:1 , name: "Ravi"};
 
   return(
   <>
     <h1>hello ravi</h1>
     <h1>PI={pi}</h1>
     <h1>{str}</h1>
-    <h1>{active ? "GM" : "GN"}</h1>
-
-    {active && <h1>GM</h1>}
-    {active ? <h1>GM</h1> : <h1>GNight</h1>}
-
-    {isUserLoggedIn && <h1>Welcome</h1>}
+    <h1>ID: {user.id}, Name: {user.name}</h1>
 
   
   </>
