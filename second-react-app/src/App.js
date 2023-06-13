64c478e2fadf00dc210d5d896feeabad02cd2f46
Raviraj patil <ravirajpatil298@gmail.com>
2023-06-13 13:59:27 +0530

    Extrenal-css-bootstrap

--- second-react-app/src/App.js
+++ second-react-app/src/App.js
@@ -1,11 +1,19 @@
-import "./App.css";
-function App() {
+import DataBlock from "./DataBlock";
+
+
+function App(){
   return (
     <>
-      <h1 className="beatiful">Hello Raj</h1>
-      <h1 style= {{color:"red"}}>hi</h1>
+    <h1 className="bg-primary text-light p-3" >My Project</h1>
+    <DataBlock/>
+    <DataBlock/>
+    <DataBlock/>
+    <DataBlock/>
+    <DataBlock/>
     </>
-  );
+
+  )
 }
+
 
 export default App;
---
+++ second-react-app/src/DataBlock.js
@@ -0,0 +1,15 @@
+function DataBlock() {
+  return (
+    <>
+      <div className="alert alert-primary">
+        <h1>Lorem5</h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Nihil, facilis explicabo possimus tempora inventore ratione quae
+        excepturi sed dolores amet, quis perspiciatis consequuntur repudiandae
+        cupiditate voluptatibus maxime nisi eum sapiente! Veritatis asperiores
+        totam, maiores fugiat consequatur natus impedit et corrupti labore culpa
+        iure commodi, vel id tempora, quaerat itaque optio?
+      </div>
+    </>
+  );
+}
+export default DataBlock;
