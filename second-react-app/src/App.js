034dfc3571e582951a65382693cdc671795aaebc
Raviraj patil <ravirajpatil298@gmail.com>
2023-06-13 10:12:54 +0530

    interpollation

--- second-react-app/src/App.js
+++ second-react-app/src/App.js
@@ -1,14 +1,14 @@
-function App() {
-  return (
-    <>
-      <h1>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae qui
-        consequatur, nesciunt magnam necessitatibus mollitia! Placeat possimus
-        sunt quia, aliquam fuga quis minima laudantium cum, aut porro distinctio
-        eveniet. Deserunt ipsam officia porro quaerat beatae eum minima
-        aspernatur mollitia incidunt?
-      </h1>
-    </>
-  );
+function App(){
+  let str =`hello ravi`;
+  return(
+  <>
+    <h1>{str}</h1>
+    <h1>hello ravi</h1>
+    <h1>str</h1>
+    <h1>{str.toUpperCase()}</h1>
+
+  
+  </>
+  )
 }
 export default App;
