f992a7d7e746ff35f11a22f556e7bedfe24ca532
Raviraj patil <ravirajpatil298@gmail.com>
2023-06-13 09:46:34 +0530

    conditional return

--- second-react-app/src/App.js
+++ second-react-app/src/App.js
@@ -1,6 +1,16 @@
 
 function App(){
-    let jsx1=<h1>Hello universe</h1>;
-    return jsx1;
+    let jsx1=<h1>good morning </h1>;
+    
+    // let jsx2= (
+    //   <>
+    //     <h1> Good night</h1>
+    //   </>
+    // );
+
+    // OR
+    let jsx2=<h1>good night</h1>
+    return true ? jsx1 : jsx2;
+
 }
 export default App;
