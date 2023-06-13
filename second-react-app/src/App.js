1f6e1c0789aa0015d0505fa66ca71172d0297b4d
Raviraj patil <ravirajpatil298@gmail.com>
2023-06-13 11:44:30 +0530

    external style

--- second-react-app/src/App.js
+++ second-react-app/src/App.js
@@ -1,14 +1,11 @@
-function App(){
-  let style = {background : "green",color : "white"}
-  return(
-     <>
-   <h1 style={style} >Hello World</h1>
-
-   <h1 style={{background : "red",color : "white"}} >Hello raviraj</h1>
-
-   <h1 style={{backgroundColor : "pink" ,color:"white",padding : "7px"}} >Raviraj</h1>
-  </>
+import "./App.css";
+function App() {
+  return (
+    <>
+      <h1 className="beatiful">Hello Raj</h1>
+      <h1 style= {{color:"red"}}>hi</h1>
+    </>
   );
 }
 
-export default App
+export default App;
