870400a4cffe95bd782850853e2b0a37f717cedd
Raviraj patil <ravirajpatil298@gmail.com>
2023-06-13 10:27:33 +0530

    interpolation

--- second-react-app/src/App.js
+++ second-react-app/src/App.js
@@ -1,11 +1,20 @@
 function App(){
   let str =`hello ravi`;
+  let pi=3.14;
+  let active=true;
+  let isUserLoggedIn = true;
+
   return(
   <>
-    <h1>{str}</h1>
     <h1>hello ravi</h1>
-    <h1>str</h1>
-    <h1>{str.toUpperCase()}</h1>
+    <h1>PI={pi}</h1>
+    <h1>{str}</h1>
+    <h1>{active ? "GM" : "GN"}</h1>
+
+    {active && <h1>GM</h1>}
+    {active ? <h1>GM</h1> : <h1>GNight</h1>}
+
+    {isUserLoggedIn && <h1>Welcome</h1>}
 
   
   </>
